f6bbd5c3d1e2c3b8ff662f963dcabd162f6850d8
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-16 16:36:55 -0300

    enum
    Criando um novo tipo de Dado com o enum

--- switch/switch/Program.cs
+++ switch/switch/Program.cs
@@ -9,31 +9,36 @@ namespace @switch
 {
     internal class Program
     {
+        enum Cor { Vermelho = 1, Azul, Verde, Amarelo} //(enum)Cria um novo tipo de dado
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite uma cor");
-            string cor = Console.ReadLine();
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1-Vermelhor\n2-Azul\n3-Verde\n4-Amarelo");
+           int opcao = int.Parse(Console.ReadLine());
+            Cor corSelecionada = (Cor)opcao;
 
-          switch (cor)
+            Console.WriteLine(corSelecionada); //Usado o indice do enum para exibir o dado
+
+          switch (corSelecionada)
             {
-                case "vermelho":
+                case Cor.Vermelho:
                     Console.WriteLine("Sua cor preferida é o Vermelho");
                     break;
-                case "azul":
+                case Cor.Azul:
                     Console.WriteLine("Sua cor preferida é o Azul");
                     break;
-                case "verde":
+                case Cor.Verde:
                     Console.WriteLine("Sua cor preferida é o Verde");
                     break;
-                case "amarelo":
+                case Cor.Amarelo:
                     Console.WriteLine("Sua cor preferida é o Amarelo");
                     break;
                 default:
                     Console.WriteLine("Não encontrei a cor. Tente de novo !");
                     break;
             };
-
-
+            
+         
             
 
             Console.ReadLine();
