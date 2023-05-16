105eeee9d8cc1f817929402c053c9a515c64e205
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-16 14:12:21 -0300

    arrays

--- arrays/arrays/Program.cs
+++ arrays/arrays/Program.cs
@@ -10,6 +10,37 @@ namespace arrays
     {
         static void Main(string[] args)
         {
+            //Jeito tradicional de criar uma arrays
+            string[] nomes = new string[3] //com limite de tamanho
+            {
+                "Antonio",
+                "Bruna",
+                "Augusto"
+            };
+            nomes[2] = "Carlos";
+            int[] idade = new int[] //sem limite de tamanho
+            {
+                36,
+                24,
+                15
+            };
+
+            Console.WriteLine(nomes[2]);
+            Console.WriteLine(idade[2]);
+
+
+            //Jeito simplificado
+            float[] num =
+            {
+                3.14f,
+            2.68f,
+            1.33f
+            };
+
+            Console.WriteLine(num[2]);
+
+
+            Console.ReadLine();
         }
     }
 }
